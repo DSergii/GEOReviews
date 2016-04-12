eee3e3e866917ee06d3c6e96df4b4c308c1fa571
Sergey Drozd <drozdsa@gmail.com>
2016-04-12 18:02:36 +0300

    add

--- js/main.js
+++ js/main.js
@@ -31,6 +31,8 @@ window.onload = function(){
     }
     myPosition();*/
 
+    var mw = $('#geo');
+
     ymaps.ready(init);
 
     function init () {
@@ -44,7 +46,11 @@ window.onload = function(){
 
         myMap.events.add('click', function (e) {
             var coords = e.get('coords');
-            console.log(coords);
+            //console.log(e.get('target'));
+            mw.modal('show');
+
+            createPoint(coords[0].toFixed(6), coords[1].toFixed(6));
+            //getAddress(coords[0].toFixed(3), coords[1].toFixed(3));
             /*if (!myMap.balloon.isOpen()) {
                 var coords = e.get('coords');
                 myMap.balloon.open(coords, {
@@ -61,6 +67,100 @@ window.onload = function(){
                 myMap.balloon.close();
             }*/
         });
+
+        function createPoint(lat, lon) {
+            console.log(lat, lon);
+            myGeoObject = new ymaps.GeoObject({
+                // Описание геометрии.
+                geometry: {
+                    type: "Point",
+                    coordinates: [lat, lon]
+                },
+                // Свойства.
+                properties: {
+                    //iconContent: 'Я тащусь',
+                    //hintContent: 'Ну давай уже тащи'
+                }
+            }, {
+                // Опции.
+                // Иконка метки будет растягиваться под размер ее содержимого.
+                preset: 'islands#blackStretchyIcon',
+                // Метку можно перемещать.
+                draggable: true
+            });
+
+            myMap.geoObjects
+                .add(myGeoObject);
+
+            
+            var myClusterer = new ymaps.Clusterer();
+            myClusterer.add(myGeoObject);
+            myMap.geoObjects.add(myClusterer);
+
+        }
+
+        function getAddress(lat, lon) {
+
+            var aaa = ymaps.geocode([lat, lon]);
+            console.log(aaa);
+            var xhr = new XMLHttpRequest();
+
+            xhr.open('GET', 'https://geocode-maps.yandex.ru/1.x/?format=json&geocode=' + lat + ',' + lon, true);
+
+            xhr.onload = function() {
+                //console.log(xhr.response);
+            }
+
+            xhr.send();
+        }
+
+        var btn = document.getElementById('submitBtn');
+        var form = document.getElementById('feadback');
+
+        btn.addEventListener('click', sendComment);
+
+        function sendComment() {
+
+            var data = {
+                "op": "add",
+                "review": {
+                    "coords": {"x": 55.76048396289834, "y": 37.58335174560545},
+                    "address": "Россия, Москва, Большая Грузинская улица, 8с36",
+                    "name": "Сергей",
+                    "place": "Шоколадница",
+                    "text": "Кругом зомби!!!!",
+                    "date": "2016.04.09 22:32:00"
+                }
+            }
+
+            var xhr = new XMLHttpRequest();
+
+
+
+            xhr.open('POST', 'http://localhost:3000/', true);
+            //xhr.setRequestHeader('Content-Type', 'application/json');
+
+            xhr.onload = function() {
+                console.log(xhr.response);
+            }
+
+            xhr.send(data);
+
+            /*$('#feadback').on('submit', function(e){
+                e.preventDefault();
+                console.log(this);
+                $.ajax({
+                    type: 'POST',
+                    url: 'some url',
+                    dataType: 'json',
+                    data: $(this).serialize(),
+                    success: function(data){
+                        console.log(data);
+                    }
+                })
+            });*/
+
+        }
     }
     
 }
